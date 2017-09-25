9b9fe7db74d611e29e908627142a317d2c711c40
malikbekkouche <malik-bekkouche@outlook.dk>
2017-09-25 16:01:19 +0200

    fixed amount of time in addition to complete auto test

--- FixedOpTest.java
+++ FixedOpTest.java
@@ -1,44 +1,82 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.ThreadLocalRandom;
 class FixedOpTest {
 	public static void main(String[] args){
 		int[] nbrThreads={1,2,4,8,16,32,64};
-		int nbrTries=10;
-		int nbrOp=new Double(Math.pow(2,25)).intValue();
+		int nbrTries=6;
+		//int nbrOp=new Double(Math.pow(2,25)).intValue();
 		int cnt=0;
 
-		//for(int i=0;i<nbrThreads.length;i++){
+		  for(int i=0;i<nbrThreads.length;i++){
 			ConcurrentChromaticTreeMap<Integer,Integer> tree=new ConcurrentChromaticTreeMap();
-			TestThread[] threads=new TestThread[nbrThreads[cnt]];
-
-			double[] times=new double[nbrTries];
+			TestThread[] threads=new TestThread[nbrThreads[i]];
+			long[] times=new long[nbrTries];
+			AtomicBoolean keepRunning=new AtomicBoolean(true);
+			ArrayList<AtomicLong> results=new ArrayList<AtomicLong>();
 			for (int k=0; k<nbrTries ; k++) {
-				for (int j=0;j<nbrThreads[cnt] ;j++ ) {
-					threads[j]=new TestThread(tree,nbrOp/nbrThreads[cnt]);
+				for (int j=0;j<nbrThreads[i] ;j++ ) {
+					AtomicLong operations=new AtomicLong(0);
+					results.add(operations);
+					threads[j]=new TestThread(tree,keepRunning,operations);
 				}
-				Timer t=new Timer();
 
-			for (int j=0;j<nbrThreads[cnt] ;j++ ) {
+
+				ExecutorService es=Executors.newSingleThreadExecutor();
+		    Future<Void> ft=es.submit(new Task());
+
+			for (int j=0;j<nbrThreads[i] ;j++ ) {
 				threads[j].start();
 			}
 			try{
-				for (int j=0;j<nbrThreads[cnt] ;j++ ) {
-					threads[j].join();
-				}
-			}catch(Exception e){}
+		      ft.get(10, TimeUnit.SECONDS);
+		  }catch(Exception e){
+		    ft.cancel(true);
+		    keepRunning.set(false);
+		   // System.out.println("Terminated!");
+		  }
 
-			double time=t.check()*1e6;
-			times[k]=time;
+		  es.shutdownNow();
+			long totalOp=0;
+			for (int j=0;j<nbrThreads[i] ;j++ ) {
+				totalOp+=results.get(j).get();
+			}
+			times[k]=totalOp;
 			System.out.println(times[k]);
 		} //tries
-		double total=0.0;
-		System.out.println("Threads : "+nbrThreads[cnt]);
+		long total=0;
+		System.out.println("Threads : "+nbrThreads[i]);
 		for (int k=0;k<nbrTries ;k++ ) {
 			total+=times[k];
 		}
 		double average=total/nbrTries;
 		System.out.println("\n"+" average : "+average);
-	//} //for trheads
+		System.out.println("throughput : "+average/10/1000);
+	  } //for trheads
 
+	}
+}
+
+class Task implements Callable<Void> {
+	public Void call(){
+		boolean t=true;
+		while(t){
+			try {
+				Thread.sleep(10);
+
+			}catch(Exception e){
+				//System.out.println("error");
+			}
+		}
+		return null;
 	}
 }
 
@@ -47,34 +85,32 @@ class TestThread extends Thread {
 	int range=1000;
 	int put=10,remove=5,get=100-put-remove;
 	int nbrOp;
+	AtomicBoolean keepRunning;
+	AtomicLong result;
 
 
-	public TestThread(ConcurrentChromaticTreeMap<Integer,Integer> tree,int nbrOp){
+	public TestThread(ConcurrentChromaticTreeMap<Integer,Integer> tree,AtomicBoolean kr,AtomicLong r){
 		this.tree=tree;
-		this.nbrOp=nbrOp;
+		keepRunning=kr;
+		result=r;
 	}
 
 	public void run(){
 		Random r=new Random();
-		for (int j=0;j<nbrOp/100 ;j++ ) {
+		while(keepRunning.get()){
 			for(int i=0;i<put;i++)
 				tree.put(r.nextInt(range+1),0);
+			result.addAndGet(put);
 			for(int i=0;i<get;i++)
 				tree.get(r.nextInt(range+1));
+			result.addAndGet(get);
 			for(int i=0;i<remove;i++)
 				tree.remove(r.nextInt(range+1));
-		}
+			result.addAndGet(remove);
+
+			}
 
 	}
 
-	public int put(){
-		return (nbrOp*put/100);
-	}
-	public int remove(){
-		return (nbrOp*remove/100);
-	}
 
-	public int get(){
-		return (nbrOp*(100-put-remove)/100);
-	}
 }
