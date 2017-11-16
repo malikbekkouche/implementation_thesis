5306f146d27937b18fc1ebde854ac633f552abef
malikbekkouche <malik-bekkouche@outlook.dk>
2017-11-16 18:07:33 +0100

    magic

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ class Main {
 		//System.out.println("put "+tree.put(8,1));
 		System.out.println("get "+tree.get(8));
 		System.out.println("remove "+tree.remove(8));
-		System.out.println("get snap "+tree.get(8));
+		System.out.println("get snap "+snap.get(8));
 		//System.out.println(tree.search(8,true).n.value);
 	}
 }
