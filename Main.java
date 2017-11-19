3295f15f8eaf00f08d238624f3d6432203e69f24
malikbekkouche <malik-bekkouche@outlook.dk>
2017-11-19 11:27:49 +0100

    replaceopsnap works now

--- Main.java
+++ Main.java
@@ -6,14 +6,16 @@ class Main {
 		System.out.println("put "+tree.put(4,5));
 		System.out.println("put "+tree.put(18,3));
 		System.out.println("put "+tree.put(1,5));
-		System.out.println("remove "+tree.remove(1));
+		//System.out.println("put "+tree.put(15,88));
+		//System.out.println("remove "+tree.remove(1));
 		ConcurrentChromaticTreeMap<Integer,Integer> snap=tree.snapshot();
-		//System.out.println("put "+tree.put(8,1));
-		//System.out.println("get "+tree.get(8));
+		System.out.println("put "+tree.put(8,1));
+		System.out.println("-------------------------------");
+		System.out.println("get "+tree.get(8));
 		//System.out.println("remove "+tree.remove(8));
 		//System.out.println("get "+tree.get(8));
-		System.out.println("update "+tree.put(8,8));
-		System.out.println("get snap "+tree.get(8));
+		//System.out.println("update "+tree.put(8,8));
+		//System.out.println("get snap "+tree.get(8));
 		//System.out.println(tree.search(8,true).n.value);
 	}
 }
